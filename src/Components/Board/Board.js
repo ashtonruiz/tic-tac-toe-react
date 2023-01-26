69c4dc1d84d87e89d0c51a9eaee4a83731cd8002
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-26 12:32:26 -0800

    mapped through boarddata so now i'm getting multiple boxes rendering

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import Box from '../Box/Box.js';
+import boardData from '../../Board-data.js';
+import './Board.css';
 
 export default function Board() {
   return (
-    <Box/>
+    <div>
+      {boardData.map((data) => (
+        <Box key={data.space}/>
+      ))} 
+    </div>
+
   );
 }
+
