b47b1ba88f74b70e7ce4cd31af477bbc754984b0
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-27 17:43:13 -0800

    added catscratch logic

--- src/Context/GameContext.js
+++ src/Context/GameContext.js
@@ -5,7 +5,7 @@ const GameContext = createContext();
 const GameProvider = ({ children }) => {
   const [board, setBoard] = useState([
     { space: 0, content: '' },
-    { space: 1, content:'' },
+    { space: 1, content: '' },
     { space: 2, content: '' },
     { space: 3, content:'' },
     { space: 4, content: '' },
@@ -22,31 +22,8 @@ const GameProvider = ({ children }) => {
     });
     setBoard(newBoard);
     handleWin();
-    // yesWin();
-    
+    checkScratch();
   };
-
-  // function handleWin(box) {
-  //   const lines = [
-  //     [0, 1, 2],
-  //     [3, 4, 5],
-  //     [6, 7, 8],
-  //     [0, 3, 6],
-  //     [1, 4, 7],
-  //     [2, 5, 8],
-  //     [0, 4, 8],
-  //     [2, 4, 6],
-  //   ];
-  //   for (let i = 0; i < lines.length; i++) {
-  //     const [a, b, c] = lines[i];
-  //     if (box[a] && box[b] === box[c] && box[a] === box[c]) {
-  //       return console.log('win');
-  //     }
-  //   }
-  //   return null;
-// const yesWin = () => {
-  // if (active === true) checkWin();
-// };
 
   const handleWin = () => {
     if ((board[0].content === board[1].content) & (board[0].content !== '') & (board[2].content === board [0].content)) setActive(false);
@@ -58,13 +35,17 @@ const GameProvider = ({ children }) => {
     if ((board[1].content === board[4].content) & (board[1].content !== '') & (board[7].content === board [1].content)) setActive(false);
     if ((board[2].content === board[5].content) & (board[2].content !== '') & (board[8].content === board [2].content)) setActive(false);
   };
-    
+  
+  const checkScratch = () => {
+    if ((board[0].content !== '') & (board[1].content !== '') & (board[2].content !== '') & (board[3].content !== '') & (board[4].content !== '') & (board[5].content !== '') & (board[6].content !== '') & (board[7].content !== '') & (board[8].content !== '')) console.log('scratch');
+  };
+
   const [currentPlayer, setCurrentPlayer] = useState('X');
   const [gameMessage, setGameMessage] = useState(null);
   const [active, setActive] = useState(true);
     
   return (
-    <GameContext.Provider value={{ handleWin, handleClick, board, setBoard, currentPlayer, setCurrentPlayer, gameMessage, setGameMessage, active, setActive }}>
+    <GameContext.Provider value={{ checkScratch, handleWin, handleClick, board, setBoard, currentPlayer, setCurrentPlayer, gameMessage, setGameMessage, active, setActive }}>
       {children}
     </GameContext.Provider>
   );
